2325eb45092c6846f820d0a038559b713d6c203f
PdcShoun <pdc.pachara@gmail.com>
2022-10-08 17:28:32 +0700

    Add kube.yaml file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# FastAPI
+# FastAPI by Kubernetes
+
+First create namespace
+```
+kubectl create ns python 
+```
+Run Run Run
+```
+kubectl apply -f kube.yaml -n python 
+```
+Stop
+```
+kubectl delete -f kube.yaml -n python
+```
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .models import People
 from dotenv import dotenv_values
 from .db import conn
@@ -11,11 +12,6 @@ config = dotenv_values(".env")
 app = FastAPI()
 connect(db='db', host='localhost', port=27017, username='admin', password='admin')
 
-@app.on_event("startup")
-def startup_db_client():
-    print("========")
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
 
 
 @app.get('/')
@@ -25,7 +21,4 @@ def home():
 
 @app.get('/get-all')
 async def get_all():
-    print("--------")
-    conn.local.user.find()
-    print(conn.local.user.find())
     return {"test": 123}
