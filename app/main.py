5f700d05dcc5f71fdeb1094f6bb1eb77c8b1d175
PdcShoun <pdc.pachara@gmail.com>
2024-01-06 16:56:30 +0700

    [FIX]error build image

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .models import People
 from dotenv import dotenv_values
-from .db import conn
-from mongoengine import connect
-from pymongo import MongoClient
-import json
 
 config = dotenv_values(".env")
 
 app = FastAPI()
-connect(db='db', host='localhost', port=27017, username='admin', password='admin')
 
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
